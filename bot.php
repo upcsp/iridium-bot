94961d4b6c7b630b4462a96f62a1d53073330659
polmes <pol.developer1@gmail.com>
2017-03-01 20:13:20 +0100

    Fix bot, first working version

--- bot.php
+++ bot.php
@@ -7,27 +7,34 @@ $attachments = array(
 	array(
 		'color' => '#3498DB',
 		'fields' => array(
-			'title' => 'GPS information',
-			'value' =>  '*Position* ' . $data[0] . ' N, ' . $data[1] . ' E\n*Altitude* ' . $data[2] . ' m'
+			array(
+				'title' => 'GPS information',
+				'value' =>  '*Position* ' . $data[0] . ' N, ' . $data[1] . ' E\n*Altitude* ' . $data[2] . ' m'
+			)
 		)
 	),
 	array(
 		'color' => '#D00000',
 		'fields' => array(
-			'title' => 'Follow live!',
-			'value' => '<https://dev.coderagora.com/iridium/|Live tracking>\n<https://www.google.com/maps/preview/@' . $data[0] . ',@' . $data[1] . ',8z|Google Maps>'
+			array(
+				'title' => 'Follow live!',
+				'value' => '<https://dev.coderagora.com/iridium/|Live tracking>\n<https://www.google.com/maps/@' . $data[0] . ',@' . $data[1] . ',15z|Google Maps>'
+			)
 		)
 	)
 );
-
+$json_data = json_encode( array( 'text' => $text, 'attachments' => $attachments ) );
 print_r( $text );
 print_r( $attachments );
-print_r( json_encode( $text, $attachments ) );
+print_r( $json_data );
 
 $ch = curl_init( 'https://hooks.slack.com/services/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' );
 curl_setopt( $ch, CURLOPT_POST, true );
-curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $text, $attachments ) );
-// curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
-// curl_setopt( $ch, CURLOPT_HEADER, 0 );
-// curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data );
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	'Content-Type: application/json',
+	'Content-Length: ' . strlen( $json_data )
+));
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 curl_exec( $ch );
